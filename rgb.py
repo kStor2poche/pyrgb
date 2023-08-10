ffb3cd182f1b1650ffb1b4389da6fe4051136517
kStor2poche <leo.lagarrigue@orange.fr>
2023-08-10 15:21:49 +0200

    Correct handling of delays and better code styling

--- rgb.py
+++ rgb.py
@@ -3,7 +3,6 @@
 
 from Xlib.display import Display
 from Xlib.ext import xinput
-import Xlib
 from PIL import ImageGrab
 from time import sleep
 import signal
@@ -21,11 +20,12 @@ def screencolor_picker():
     root.xinput_select_events([(xinput.AllDevices, 
                                 xinput.ButtonPressMask)])
     # wait for mouse click
-    print("Click on the spot you wan to take a sample from...")
+    print("Click on the spot you want to take a sample from...")
     event = display.next_event()
     coord = root.query_pointer()._data
     x = coord["root_x"]
     y = coord["root_y"]
+    display.close()
     img = ImageGrab.grab()
     return str(list(img.getpixel((x,y)))).replace('[','').replace(']','')
 
@@ -51,126 +51,106 @@ def normtohex(n) :
         htemp = hex(round(n[i]*255))[2:]
         h = h + ("0"+htemp)*(len(htemp)!=2) + (htemp)*(len(htemp)==2) #avoids an if statement
     return h
-
-
-## Main program flow
-if len(sys.argv)==1 :
-    inpt = input("Enter the color value that you want to convert > ")
-else :
-    inpt = sys.argv[1]
-    if (inpt=="-h") or (inpt=="--help") :
-        print("Usage : rgb { -h | -p | (color) }\n\n\
---help\n\
--h      Show this help message :)\n\
-\n\
--p      Pick color from current screen. Appending the option with a number\n\
-        will add [number] seconds of delay before user is prompted to choose color.\n\
-\n\
-(color) A color among any of these three types of data : \n\
-    - A hexadecimal value in the form of \"#hhhhhh\" or \"hhhhhh\"\n\
-    - An array of normalized values in the form of \"d.d,d.d,d.d\"\n\
-    - An array of decimal 8 bit values in the form of \"d,d,d\"\n\n\
-Note that the arrays can have a dimension different to 3")
-        exit(0)
-    elif (len(inpt)>=2 and inpt[0:2]=="-p") :
-        delay = "none"
-        if (len(sys.argv) >= 3) :
-            delay = sys.argv[2]
-        elif (len(inpt) > 2) :
-            delay = inpt[2:]
-        if (delay in "0123456789") :
-            print("Waiting ",delay," second","s"*(int(delay)>1)," before accepting for user input...", sep='')
-            sleep(int(delay))
-        inpt = screencolor_picker()
-
-if len(inpt)==0 :
-    print("You need to provide a value. See rgb.py -h for the different forms of input accepted.", file=sys.stderr)
-    exit(-1)
-
-
-
-if "." in inpt :
-    #type = norm
-    inpt=inpt.split(",")
-    inpt=list(map(float,inpt))
-    for i in inpt : 
-        if i > 1. or i < 0. :
-            print("Error : input value",i,"out of range for a normalized decimal color !", file=sys.stderr)
-            exit(1)
-    print("\nInput identified as normalized values",inpt)
-    h=normtohex(inpt)
-    bit=hexto8bit(h)
-    print(f"\n• Hexadecimal :          #{h}")
-    print(f"• 8 bit decimal values : {bit}".replace(']','').replace('[',''))
+def print_color_squares(bit) :
     if len(bit) >= 3 :
         print(f"\n                           \
-{colors.BLK}  🭁████🭌\
-{colors.GRY}  🭁████🭌\
-{colors.WHT}  🭁████🭌\
-{colors.RST}")
+    {colors.BLK}  🭁████🭌\
+    {colors.GRY}  🭁████🭌\
+    {colors.WHT}  🭁████🭌\
+    {colors.RST}")
         print(f"The color looks like this :\
-{colors.BLK}  ██\033[38;2;{bit[0]};{bit[1]};{bit[2]}m██{colors.BLK}██\
-{colors.GRY}  ██\033[38;2;{bit[0]};{bit[1]};{bit[2]}m██{colors.GRY}██\
-{colors.WHT}  ██\033[38;2;{bit[0]};{bit[1]};{bit[2]}m██{colors.WHT}██")
+    {colors.BLK}  ██\033[38;2;{bit[0]};{bit[1]};{bit[2]}m██{colors.BLK}██\
+    {colors.GRY}  ██\033[38;2;{bit[0]};{bit[1]};{bit[2]}m██{colors.GRY}██\
+    {colors.WHT}  ██\033[38;2;{bit[0]};{bit[1]};{bit[2]}m██{colors.WHT}██")
         print(f"                           \
-{colors.BLK}  🭒████🭝\
-{colors.GRY}  🭒████🭝\
-{colors.WHT}  🭒████🭝\
-{colors.RST}\n")
-elif "," in inpt :
-    #type = 8bit
-    inpt=inpt.split(",")
-    inpt=list(map(int,inpt))
-    for i in inpt : 
-        if i > 255 or i < 0 :
-            print("Error : input value",i,"out of range for an 8 bit decimal color !", file=sys.stderr)
-            exit(1)
-    print("\nInput identified as 8 bit decimal values",inpt)
-    n=bit8tonorm(inpt)
-    h=normtohex(n)
-    print(f"\n• Normalized values : {n}".replace(']','').replace('[',''))
-    print(f"• Hexadecimal :       #{h}")
-    if len(inpt) >= 3 :
-        print(f"\n                           \
-{colors.BLK}  🭁████🭌\
-{colors.GRY}  🭁████🭌\
-{colors.WHT}  🭁████🭌\
-{colors.RST}")
-        print(f"The color looks like this :\
-{colors.BLK}  ██\033[38;2;{inpt[0]};{inpt[1]};{inpt[2]}m██{colors.BLK}██\
-{colors.GRY}  ██\033[38;2;{inpt[0]};{inpt[1]};{inpt[2]}m██{colors.GRY}██\
-{colors.WHT}  ██\033[38;2;{inpt[0]};{inpt[1]};{inpt[2]}m██{colors.WHT}██")
-        print(f"                           \
-{colors.BLK}  🭒████🭝\
-{colors.GRY}  🭒████🭝\
-{colors.WHT}  🭒████🭝\
-{colors.RST}\n")
-else :
-    #type = hex
-    if inpt[0]=='#' :
-        inpt=inpt[1:]
-    for i in inpt : 
-        if not(i in "0123456789abcdefABCDEF") :
-            print("Error : input value",i,"out of range for a hexadecimal color !", file=sys.stderr)
-            exit(1)
-    inpt=inpt.lower()
-    print("\nInput identified as hex value #",inpt,sep='')
-    bit=hexto8bit(inpt)
-    n=bit8tonorm(bit)
-    print(f"\n• 8 bit decimal values : {bit}".replace(']','').replace('[',''))
-    print(f"• Normalized values :    {n}".replace(']','').replace('[',''))
-    if len(bit) >= 3 :
-        print(f"\n                           \
-{colors.BLK}  🭁████🭌\
-{colors.GRY}  🭁████🭌\
-{colors.WHT}  🭁████🭌\
-{colors.RST}")
-        print(f"The color looks like this :\
-{colors.BLK}  ██\033[38;2;{bit[0]};{bit[1]};{bit[2]}m██{colors.BLK}██\
-{colors.GRY}  ██\033[38;2;{bit[0]};{bit[1]};{bit[2]}m██{colors.GRY}██\
-{colors.WHT}  ██\033[38;2;{bit[0]};{bit[1]};{bit[2]}m██{colors.WHT}██")
-        print(f"                           \
-{colors.BLK}  🭒████🭝\
-{colors.GRY}  🭒████🭝\
-{colors.WHT}  🭒████🭝\
-{colors.RST}\n")
+    {colors.BLK}  🭒████🭝\
+    {colors.GRY}  🭒████🭝\
+    {colors.WHT}  🭒████🭝\
+    {colors.RST}\n")
+    if len(bit) > 3:
+        print("(only the first three values were taken into account)")
+
+
+def main() :
+    if len(sys.argv)==1 :
+        inpt = input("Enter the color value that you want to convert > ")
+    else :
+        inpt = sys.argv[1]
+        if (inpt=="-h") or (inpt=="--help") :
+            print("Usage : rgb { -h | -p | (color) }\n\n\
+    --help\n\
+    -h      Show this help message :)\n\
+    \n\
+    -p      Pick color from current screen. Appending the option with a number\n\
+            will add [number] seconds of delay before user is prompted to choose color.\n\
+    \n\
+    (color) A color among any of these three types of data : \n\
+        - A hexadecimal value in the form of \"#hhhhhh\" or \"hhhhhh\"\n\
+        - An array of normalized values in the form of \"d.d,d.d,d.d\"\n\
+        - An array of decimal 8 bit values in the form of \"d,d,d\"\n\n\
+    Note that the arrays can have a dimension different to 3")
+            exit(0)
+        elif (len(inpt)>=2 and inpt[0:2]=="-p") :
+            delay = "none"
+            if (len(sys.argv) >= 3) :
+                delay = sys.argv[2]
+            elif (len(inpt) > 2) :
+                delay = inpt[2:]
+            if (delay.isdigit()) : #not working properly for double digits
+                print("Waiting ",int(delay)," second","s"*(int(delay)>1)," before accepting for user input...", sep='')
+                sleep(int(delay))
+            else :
+                print("Delay\"", delay, "\"invalid.")
+            inpt = screencolor_picker()
+
+    if len(inpt)==0 :
+        print("You need to provide a value. See rgb.py -h for the different forms of input accepted.", file=sys.stderr)
+        exit(-1)
+
+    if "." in inpt :
+        #type = norm
+        inpt=inpt.split(",")
+        inpt=list(map(float,inpt))
+        for i in inpt : 
+            if i < 0. or i > 1. :
+                print("Error : input value",i,"out of range for a normalized decimal color !", file=sys.stderr)
+                exit(1)
+        print("\nInput identified as normalized values",inpt)
+        h=normtohex(inpt)
+        bit=hexto8bit(h)
+        print(f"\n• Hexadecimal :          #{h}")
+        print(f"• 8 bit decimal values : {bit}".replace(']','').replace('[',''))
+        print_color_squares(bit)
+    elif "," in inpt :
+        #type = 8bit
+        inpt=inpt.split(",")
+        inpt=list(map(int,inpt))
+        for i in inpt : 
+            if i > 255 or i < 0 :
+                print("Error : input value",i,"out of range for an 8 bit decimal color !", file=sys.stderr)
+                exit(1)
+        print("\nInput identified as 8 bit decimal values",inpt)
+        bit=inpt
+        n=bit8tonorm(inpt)
+        h=normtohex(n)
+        print(f"\n• Normalized values : {n}".replace(']','').replace('[',''))
+        print(f"• Hexadecimal :       #{h}")
+        print_color_squares(bit)
+    else :
+        #type = hex
+        if inpt[0]=='#' :
+            inpt=inpt[1:]
+        for i in inpt : 
+            if not(i in "0123456789abcdefABCDEF") :
+                print("Error : input value",i,"out of range for a hexadecimal color !", file=sys.stderr)
+                exit(1)
+        inpt=inpt.lower()
+        print("\nInput identified as hex value #",inpt,sep='')
+        bit=hexto8bit(inpt)
+        n=bit8tonorm(bit)
+        print(f"\n• 8 bit decimal values : {bit}".replace(']','').replace('[',''))
+        print(f"• Normalized values :    {n}".replace(']','').replace('[',''))
+        print_color_squares(bit)
+
+if __name__=="__main__" :
+    main()
